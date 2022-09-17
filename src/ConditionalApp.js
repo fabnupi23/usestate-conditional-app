c727a4e81c8caecc22ce860dc617d1d6c108ec0a
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-17 13:52:30 -0500

    Estructura del ConditionalApp con useState

---
+++ src/ConditionalApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ConditionalApp() {
+  return (
+    <div>
+        <h1>
+            ConditionalApp
+        </h1>
+    </div>
+  )
+}
+
+export { ConditionalApp };
